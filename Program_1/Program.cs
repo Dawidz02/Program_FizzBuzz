b84c77d1a34fa8bffbf7cb47781cfc98d93fb30e
Admin <Admin@DESKTOP-6SFA8O7>
2024-02-29 22:36:57 +0100

    Dodaj pliki projektów.

---
+++ Program_1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Program_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wprowadź wartosc graniczną: ");
+            int uB = int.Parse(Console.ReadLine());
+            FizzBuzz F1 = new FizzBuzz(uB);
+            F1.Display();
+;        }
+    }
+}
